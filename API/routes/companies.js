80d96cb72510323f4296582ae97b3ec9394d8004
Jean <spyroo913@hotmail.com>
2018-03-04 17:34:40 +0100

    api work (state removed)

--- API/routes/companies.js
+++ API/routes/companies.js
@@ -123,7 +123,6 @@ router.post('/register', function(req, res){
                 streetNumber: streetNumber,
                 route: route,
                 city: city,
-                state: state,
                 zipCode: zipCode,
                 country: country
             }).then(function(newPlace){
